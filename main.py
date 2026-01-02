453052abf25afb3c0e5acc9d1e86143cebcd1684
Ken Harmon <harmkenn@gmail.com>
2026-01-02 08:48:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,17 @@ import importlib.util
 import os
 
 # Set the page layout to wide
-st.set_page_config(layout="wide", page_title=f"Python Madness 2026")
+st.set_page_config(layout="wide", page_title=f"Gedcoms")
 
 # Dictionary that maps .py filenames to user-friendly names
 sub_app_names = {
-    'a_allgames.py': 'All Games',
-    'b_brackets.py': 'All Brackets',
-    'c_seedhistory.py': 'Seed History',
-    'd_teampase.py': 'Team Performance',
-    'd_teamwins.py': 'Team Wins',
-    'e_teamrank.py': 'Team Rankings',
-    'f_backpredict.py': 'Back Predict',
-    'g_fullpredict.py': 'Full Predict',
-    'h_update.py': 'Update Data',
-    'i_bracketmaker.py': 'Bracket Maker'
+    'FamilySearchTendril.py'; 'Family Search Tendril',
 }
 
 # Get a list of .py files from the SubApps folder
 sub_apps_folder = os.path.join(os.path.dirname(__file__), 'apps')
 sub_apps = [f for f in os.listdir(sub_apps_folder) if f.endswith('.py')]
-st.sidebar.title("Python Madness v2026.1")
+st.sidebar.title("Gedcoms v2026.1")
 st.sidebar.subheader("by Ken Harmon")
 # Create radio buttons in the sidebar using the user-friendly names
 selected_sub_app_name = st.sidebar.radio('', list(sub_app_names.values()))
