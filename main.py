2f0adc8c006daf26045b04a077c65eb8c0925fd5
Ken Harmon <harmkenn@gmail.com>
2026-01-02 08:46:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,35 @@ import importlib.util
 import os
 
 # Set the page layout to wide
-st.set_page_config(layout="wide", page_title=f"Gedcoms")
+st.set_page_config(layout="wide", page_title=f"Python Madness 2026")
 
 # Dictionary that maps .py filenames to user-friendly names
 sub_app_names = {
-'gedcom.py': 'Generic Gedcom', 
-    'GedcomFilter.py': 'Special Gedcom', 
-    'AncestryTendril.py': 'Ancestry Tendril', 
-    'FamilySearchTendril.py': 'Family Search Tendril',
+    'a_allgames.py': 'All Games',
+    'b_brackets.py': 'All Brackets',
+    'c_seedhistory.py': 'Seed History',
+    'd_teampase.py': 'Team Performance',
+    'd_teamwins.py': 'Team Wins',
+    'e_teamrank.py': 'Team Rankings',
+    'f_backpredict.py': 'Back Predict',
+    'g_fullpredict.py': 'Full Predict',
+    'h_update.py': 'Update Data',
+    'i_bracketmaker.py': 'Bracket Maker'
 }
 
 # Get a list of .py files from the SubApps folder
-sub_apps_folder = 'apps'
+sub_apps_folder = os.path.join(os.path.dirname(__file__), 'apps')
 sub_apps = [f for f in os.listdir(sub_apps_folder) if f.endswith('.py')]
-
+st.sidebar.title("Python Madness v2026.1")
+st.sidebar.subheader("by Ken Harmon")
 # Create radio buttons in the sidebar using the user-friendly names
-selected_sub_app_name = st.sidebar.radio('Select a sub-app', list(sub_app_names.values()))
+selected_sub_app_name = st.sidebar.radio('', list(sub_app_names.values()))
 
 # Get the corresponding .py filename from the selected name
 selected_sub_app = [k for k, v in sub_app_names.items() if v == selected_sub_app_name][0]
 
 # Import and run the selected sub-app
 if selected_sub_app:
-spec = importlib.util.spec_from_file_location(selected_sub_app, os.path.join(sub_apps_folder, selected_sub_app)) 
-sub_app_module = importlib.util.module_from_spec(spec) spec.loader.exec_module(sub_app_module)
+    spec = importlib.util.spec_from_file_location(selected_sub_app, os.path.join(sub_apps_folder, selected_sub_app))
+    sub_app_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(sub_app_module)
